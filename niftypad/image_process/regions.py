189261414a7d534dcb844a35adec10f5c38b154e
Jieqing Jiao <j.jiao@ucl.ac.uk>
2019-05-19 00:51:37 +0100

    add extract_regional_values_image_file

--- niftypad/image_process/regions.py
+++ niftypad/image_process/regions.py
@@ -2,6 +2,7 @@ __author__ = 'jieqing jiao'
 __email__ = "jieqing.jiao@gmail.com"
 
 import numpy as np
+import nibabel as nib
 
 
 def extract_regional_values(image, parcellation, labels):
@@ -16,6 +17,20 @@ def labels_to_index(parcellation, labels):
     for i in range(len(labels)):
         idx = np.logical_or(idx, parcellation == labels[i])
     return idx
+
+
+def extract_regional_values_image_file(image_file, parcellation_file):
+    image = nib.load(image_file)
+    image_data = image.get_data()
+    parcellation_img = nib.load(parcellation_file)
+    parcellation = parcellation_img.get_data()
+    regions_label = np.unique(parcellation)
+    regions_data = np.zeros(regions_label.size)
+    for i in range(regions_label.size):
+        regions_data[i] = extract_regional_values(image_data, parcellation, [regions_label[i]])
+    return regions_data, regions_label
+
+
 
 
 
