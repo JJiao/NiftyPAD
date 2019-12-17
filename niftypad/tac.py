761db6b7a3ce4d92f245cc689f08c0a75ae05c35
Jieqing Jiao <j.jiao@ucl.ac.uk>
2019-12-17 11:57:06 +0000

    add run_exp1, run_exp2 for reference tac fitting

--- niftypad/tac.py
+++ niftypad/tac.py
@@ -20,7 +20,7 @@ class TAC:
         model = getattr(models, model_name)
         kps = model(self.tac, **model_inputs)
         km_results.update(kps)
-        self.km_results.append(km_results)
+        self.km_results = km_results
 
 
 class Ref:
@@ -31,6 +31,8 @@ class Ref:
         self.inputf1_fs = []
         self.inputf1 = []
         self.inputf1cubic = []
+        self.inputf1_exp1 = []
+        self.inputf1_exp2 = []
 
     def run_feng_srtm(self, w=None):
         self.inputf1_fs, _ = models.feng_srtm(self.tac, self.dt, w=w, fig=True)
@@ -42,3 +44,19 @@ class Ref:
     def interp_1cubic(self):
         mft = kt.dt2mft(self.dt)
         self.inputf1cubic = kt.interpt1cubic(mft, self.tac, self.dt)
+
+    def run_exp1(self, w=None, idx_to_fit=None, fill_in_seconds=None):
+        self.interp_1(self)
+        self.inputf1_exp1 = self.inputf1
+        if idx_to_fit is not None:
+            inputf1_exp1, _ = models.exp_1(self.tac, self.dt, idx=idx_to_fit, w=w, fig=True)
+        if fill_in_seconds is not None:
+            self.inputf1_exp1[fill_in_seconds] = inputf1_exp1[fill_in_seconds]
+
+    def run_exp2(self, w=None, idx_to_fit=None, fill_in_seconds=None):
+        self.interp_1(self)
+        self.inputf1_exp1 = self.inputf1
+        if idx_to_fit is not None:
+            inputf1_exp2, _ = models.exp_2(self.tac, self.dt, idx=idx_to_fit, w=w, fig=True)
+        if fill_in_seconds is not None:
+            self.inputf1_exp2[fill_in_seconds] = inputf1_exp2[fill_in_seconds]
