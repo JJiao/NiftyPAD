b27f3230d3b5a82ea68d3df343d090844495a256
JJiao <JJiao@users.noreply.github.com>
2023-05-03 09:52:50 +0100

    put 'pet_image_fit' into 'parametric_images_dict'
    put 'pet_image_fit' into 'parametric_images_dict' in 'image_to_parametric'

--- niftypad/image_process/parametric_image.py
+++ niftypad/image_process/parametric_image.py
@@ -40,9 +40,9 @@ def image_to_parametric(pet_image, dt, model_name, model_inputs, km_outputs, mas
             parametric_images[p][mask[i][0], mask[i][1], mask[i][2], ] = tac.km_results[km_outputs[p].lower()]
     parametric_images_dict = dict(zip(km_outputs, parametric_images))
     if 'tacf' in tac.km_results:
-        return parametric_images_dict, pet_image_fit
+        parametric_images_dict.update({'fit': pet_image_fit})
+        # return parametric_images_dict, pet_image_fit
     return parametric_images_dict
-
 
 def image_to_parametric_files(pet_image_file, dt, model_name, model_inputs, km_outputs, mask_file=None, thr=0.005,
                               save_path=None):
